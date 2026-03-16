f8031fa723782684e72db5dd4882430e0cd0ad62
Muhammad Hasan Butt <hasan.butt2005@gmail.com>
2026-03-16 23:58:50 +0500

    Added CODEOWNERS File and added some configurations scripts

---
+++ intellecta-backend/src/main/java/com/intellecta/intellecta_backend/IntellectaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.intellecta.intellecta_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellectaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellectaBackendApplication.class, args);
+	}
+
+}
---
+++ intellecta-backend/src/test/java/com/intellecta/intellecta_backend/IntellectaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellecta.intellecta_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellectaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
