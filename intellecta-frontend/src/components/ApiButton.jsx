01e8c20ef1880f4dae963e6b53ec13ae36ad897f
Muhammad Hasan Butt <hasan.butt2005@gmail.com>
2026-04-13 00:54:10 +0500

    Testing git push

--- intellecta-frontend/src/components/ApiButton.jsx
+++ intellecta-frontend/src/components/ApiButton.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 // This is a "Dumb" component. It just takes an 'onClick' and 'label' as Props.
 function ApiButton({ onClick, label }) {
   return (
-    <button 
+    <button
       onClick={onClick}
-      className="mt-6 px-8 py-3 bg-gradient-to-r from-cyan-500 to-blue-600 text-white rounded-full font-bold shadow-lg hover:scale-105 transition-transform active:scale-95"
+      className="mt-6 px-8 py-3 bg-gradient-to-r from-red-500 to-blue-600 text-white rounded-full font-bold shadow-lg hover:scale-105 transition-transform active:scale-95"
     >
       {label}
     </button>
