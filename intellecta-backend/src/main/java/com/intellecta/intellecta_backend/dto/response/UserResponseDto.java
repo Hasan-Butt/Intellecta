020cf558c576a7ddd9c0fccfa6caab04de02c498
Muhammad Hasan Butt <hasan.butt2005@gmail.com>
2026-07-24 22:22:35 +0500

    Fixed the API Security Analysis: User Profile Endpoint and accessability findings

--- intellecta-backend/src/main/java/com/intellecta/intellecta_backend/dto/response/UserResponseDto.java
+++ intellecta-backend/src/main/java/com/intellecta/intellecta_backend/dto/response/UserResponseDto.java
@@ -1,11 +1,13 @@
 package com.intellecta.intellecta_backend.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @Builder @AllArgsConstructor @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponseDto {
     private Long id;
     private String username;
