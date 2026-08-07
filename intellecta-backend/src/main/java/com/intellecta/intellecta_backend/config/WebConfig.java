323a722a58be85613b3b791dbf00109be913da87
Plagiaryze2 <muhammadanas1243@gmail.com>
2026-08-07 19:12:25 +0500

    Security Fix #13.

--- intellecta-backend/src/main/java/com/intellecta/intellecta_backend/config/WebConfig.java
+++ intellecta-backend/src/main/java/com/intellecta/intellecta_backend/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.intellecta.intellecta_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,16 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer resourceHandlerConfigurer() {
         return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-
-            // ADD THIS — serves files from the uploads/ folder on disk
+            // serves files from the uploads/ folder on disk
             @Override
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
                 registry.addResourceHandler("/uploads/**")
